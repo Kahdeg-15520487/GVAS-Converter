2cb43718743b4dcd911ebbcb2ffb6291a0646b12
HunterStanton <ihatecompvir@icloud.com>
2017-04-20 05:14:58 -0400

    Added default info message

--- GVAS-Converter/Program.cs
+++ GVAS-Converter/Program.cs
@@ -10,6 +10,11 @@ namespace GVAS_Converter
     {
         static void Main(string[] args)
         {
+            if (args.Length != 2)
+            {
+                System.Console.WriteLine("GVAS-Converter\nA program for converting Unreal Engine 4 GVAS config files to JSON and back.\n\nUsage: \n- Converting to JSON: GVAS-Converter <savegame.bin> <output.json>\n- Converting from JSON: GVAS-Converter <input.json> <savegame.bin>\n\nShould support most UE4 games. Please file an issue on GitHub if it does not and support will be added promptly.");
+                return;
+            }
         }
     }
 }
