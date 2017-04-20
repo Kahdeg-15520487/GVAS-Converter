1dec3f7762fa0930acc6c500f160e5166ed4b66f
HunterStanton <ihatecompvir@icloud.com>
2017-04-20 05:26:03 -0400

    Update assemblyinfo

--- GVAS-Converter/Properties/AssemblyInfo.cs
+++ GVAS-Converter/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GVAS-Converter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A utility to convert Unreal Engine 4 GVAS configuration files to JSON and back.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GVAS-Converter")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Hunter Stanton 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
